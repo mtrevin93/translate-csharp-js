d508ba2270aa67a50bec03922518aba473f827ce
Michael Trevino <mtrevin93@gmail.com>
2021-08-23 12:53:47 -0400

    enemies translate c# - JS complete

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -1,1 +1,26 @@
 // Put your code here
+
+console.log("My Enemies List!");
+console.log("----------------");
+
+const makeEnemy = (firstName, lastName, offenses, isReallyHated) => {
+    const enemy = {
+                firstName: firstName,
+                lastName: lastName,
+                offenses: offenses,
+                isReallyHated: isReallyHated
+                }
+    return enemy
+}
+
+const enemies = []
+
+enemies.push(makeEnemy("Joshua","Flowers",["Being a jerk to me in elementary school","Not being nice to me in elementary school"], true))
+enemies.push(makeEnemy("Darth","Vader",["Cut off Luke's hand","Murdered all those kids","Unkind management practices"],false))
+enemies.push(makeEnemy("Betty","Rudelady",["Phone calls in the theater","Phone calls on the bus","Phone calls in line at the grovery store","Poor conversationalist"],true))
+enemies.push(makeEnemy("Leon","Peck",["Keeps giving me a hotplace"],false))
+
+enemies.map(enemy => {
+    {enemy.isReallyHated ? console.log(`${enemy.firstName} ${enemy.lastName} (Really, really dislike!)`)
+    : console.log( `${enemy.firstName} ${enemy.lastName}`);}
+})
