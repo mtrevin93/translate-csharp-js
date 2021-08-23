3b5bac83ebfc852d8c737ac1a842f9d5b3bffc9b
Michael Trevino <mtrevin93@gmail.com>
2021-08-23 14:34:21 -0400

    dice roller converted from C# to JS

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,1 +1,24 @@
 // Put your code here
+
+console.log("Let's roll some dice, baby!")
+console.log("---------------------------")
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const rollDie = () => getRandomInt(1, 6)
+
+for (i = 0; i< 10; i++){
+    const die1 = rollDie()
+    const die2 = rollDie()
+
+    let message = `${die1.toString()} + ${die2.toString()} == ${die1+die2}`
+    if (die1 === die2){
+        message += " DOUBLES!";
+    }
+    console.log(message)
+}
+
