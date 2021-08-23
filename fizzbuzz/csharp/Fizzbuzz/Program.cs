381ffcc21a1c9b148f98f56a91c56bcd37644436
Michael Trevino <mtrevin93@gmail.com>
2021-08-23 18:49:51 -0400

    fizzbuzz from JS to C#

---
+++ fizzbuzz/csharp/Fizzbuzz/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+for (int n=1; n<100; n++) {
+    if (n % 15 ==0)
+        {
+            Console.WriteLine("fizzbuzz");
+        }
+    else if (n % 3 == 0)
+        {
+            Console.WriteLine("fizz");
+        }
+
+    else if (n % 5 == 0)
+        {
+            Console.WriteLine("buzz");
+        }
+    else
+        {
+            Console.WriteLine(n);
+        }
+};
